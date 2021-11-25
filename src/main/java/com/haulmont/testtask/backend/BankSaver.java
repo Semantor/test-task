7fabd746224fd5bae23e248b67422b3252901bb2
fred <frederico666@yandex.ru>
2021-11-25 20:46:04 +0400

    update bank saver docs

--- src/main/java/com/haulmont/testtask/backend/BankSaver.java
+++ src/main/java/com/haulmont/testtask/backend/BankSaver.java
@@ -1,13 +1,15 @@
 package com.haulmont.testtask.backend;
 
 import com.haulmont.testtask.model.entity.Bank;
+import org.jetbrains.annotations.Nullable;
 
 public interface BankSaver {
     /**
      * trying to save bank in db
      * validate it
-     * @param bank to save
+     * check for null Bank, bank_id.
+     *
      * @throws com.haulmont.testtask.backend.excs.CreateBankException due to fail
      */
-    void save(Bank bank);
+    void save(@Nullable Bank bank);
 }
