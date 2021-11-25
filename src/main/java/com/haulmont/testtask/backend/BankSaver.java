d6b0843d6b26ab601d20cbe80156f0101fcbae45
fred <frederico666@yandex.ru>
2021-11-25 16:28:10 +0400

    update bank saver docs

--- src/main/java/com/haulmont/testtask/backend/BankSaver.java
+++ src/main/java/com/haulmont/testtask/backend/BankSaver.java
@@ -3,6 +3,11 @@ package com.haulmont.testtask.backend;
 import com.haulmont.testtask.model.entity.Bank;
 
 public interface BankSaver {
-
+    /**
+     * trying to save bank in db
+     * validate it
+     * @param bank to save
+     * @throws com.haulmont.testtask.backend.excs.CreateBankException due to fail
+     */
     void save(Bank bank);
 }
