4ff0bd5207ae4fcbd3852fedc866b81d451c7cce
fred <frederico666@yandex.ru>
2021-11-25 16:23:46 +0400

    update bank provider docs

--- src/main/java/com/haulmont/testtask/backend/BankProvider.java
+++ src/main/java/com/haulmont/testtask/backend/BankProvider.java
@@ -2,11 +2,27 @@ package com.haulmont.testtask.backend;
 
 import com.haulmont.testtask.model.entity.Bank;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 public interface BankProvider {
+    /**
+     * search bank by its id
+     *
+     * @param UUID bank id
+     */
     Optional<Bank> getBank(String UUID);
+
+    /**
+     * prepare list of all banks
+     *
+     * @return all banks list or {@link Collections#emptyList()}
+     */
     List<Bank> getAllBanks();
+
+    /**
+     * add to target bank all its clients in {@link Bank#getClients()} field
+     */
     void updateClients(Bank bank);
 }
