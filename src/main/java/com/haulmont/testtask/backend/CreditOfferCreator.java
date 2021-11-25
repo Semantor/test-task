351519b7d6cba811654b5f0d82a2c39a89bdbf7f
fred <frederico666@yandex.ru>
2021-11-26 03:49:20 +0400

    create credit offer service

---
+++ src/main/java/com/haulmont/testtask/backend/CreditOfferCreator.java
@@ -0,0 +1,28 @@
+package com.haulmont.testtask.backend;
+
+import com.haulmont.testtask.model.entity.Credit;
+import com.haulmont.testtask.model.entity.CreditOffer;
+import org.jetbrains.annotations.NotNull;
+
+public interface CreditOfferCreator {
+    /**
+     * UUID must be not null and not present in db.
+     * credit must be not null and present in db.
+     * same for client.
+     * credit amount must be less than {@link Credit#getCreditLimit()}
+     * and more than {@link com.haulmont.testtask.Config#CREDIT_LIMIT_MIN_VALUE}
+     *
+     * {@link CreditOffer#payments} must be:
+     * present
+     * connect to credit offer
+     * first payment with date >= today
+     *
+     * all the payment will calculate into this method and compare with {@link CreditOffer#payments}
+     * if any fields differ, failed
+     *
+     * this method also save this payment list.
+     *
+     * @throws com.haulmont.testtask.backend.excs.CreateCreditOfferException
+     */
+    void save(@NotNull CreditOffer creditOffer);
+}
