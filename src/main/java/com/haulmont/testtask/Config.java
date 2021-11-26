247573e4f35601007715f0be89c1f230fbae2455
fred <frederico666@yandex.ru>
2021-11-26 19:37:56 +0400

    update config with new implementation

--- src/main/java/com/haulmont/testtask/Config.java
+++ src/main/java/com/haulmont/testtask/Config.java
@@ -1,7 +1,11 @@
 package com.haulmont.testtask;
 
+import com.haulmont.testtask.backend.*;
+import com.haulmont.testtask.backend.impls.*;
+import com.haulmont.testtask.model.repositories.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
 import javax.annotation.PostConstruct;
@@ -31,6 +35,74 @@ public class Config {
         CREDIT_LIMIT_MIN_VALUE = new BigDecimal(minCreditLimit);
         CREDIT_RATE_MAX_VALUE = new BigDecimal(maxCreditRate);
         CREDIT_RATE_MIN_VALUE = new BigDecimal(minCreditRate);
+    }
+
+    @Bean
+    AnnuityCreditCalculator annuityCreditCalculator() {
+        return new AnnuityCreditCalculatorImpl();
+    }
+
+    @Bean
+    ClientProvider clientProvider(ClientRepository clientRepository, Validator validator) {
+        return new ClientProviderImpl(clientRepository, validator);
+    }
+
+    @Bean
+    Validator validator() {
+        return new Validator();
+    }
+
+    @Bean
+    CreditOfferProviderByClient creditOfferProvider(CreditOfferRepository creditOfferRepository,ClientRepository clientRepository) {
+        return new CreditOfferProviderByClientImpl(creditOfferRepository,clientRepository);
+    }
+
+    @Bean
+    CreditOfferCreator creditOfferCreator(CreditOfferRepository creditOfferRepository,
+                                          CreditRepository creditRepository,
+                                          ClientRepository clientRepository,
+                                          PaymentCalculator paymentCalculator,
+                                          Validator validator,
+                                          PaymentRepository paymentRepository) {
+        return new CreditOfferCreatorWithExceptionImpl(creditOfferRepository, creditRepository,
+                clientRepository, paymentCalculator, validator, paymentRepository);
+    }
+
+    @Bean
+    ClientFieldsValidator clientValidator(Validator validator) {
+        return new ClientFieldsValidatorImpl(validator);
+    }
+
+    @Bean
+    ClientSaver clientSaver(ClientRepository clientRepository, ClientFieldsValidator clientFieldsValidator, Validator validator) {
+        return new ClientSaverWithExceptionImpl(clientFieldsValidator, clientRepository, validator);
+    }
+
+    @Bean
+    BankProvider bankProvider(BankRepository bankRepository, Validator validator) {
+        return new BankProviderImpl(bankRepository, validator);
+    }
+
+    @Bean
+    BankSaver bankSaver(BankRepository bankRepository) {
+        return new BankSaverImpl(bankRepository);
+    }
+
+    @Bean
+    CreditProvider creditProvider(CreditRepository creditRepository,
+                                  Validator validator) {
+        return new CreditProviderImpl(creditRepository, validator);
+    }
+
+    @Bean
+    CreditSaver creditSaver(CreditRepository creditRepository, BankRepository bankRepository, Validator validator) {
+        return new CreditSaverImpl(creditRepository, bankRepository, validator);
+    }
+
+    @Bean
+    PaymentCalculator paymentCalculator(AnnuityCreditCalculator annuityCreditCalculator,
+                                        Validator validator) {
+        return new PaymentCalculatorImpl(annuityCreditCalculator, validator);
     }
 
 
