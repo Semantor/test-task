f7ffe3062d85f4fbdcb8d8d88d6323913c94121e
fred <frederico666@yandex.ru>
2021-11-26 17:58:09 +0400

    frontend configuration

---
+++ src/main/java/com/haulmont/testtask/ViewConfig.java
@@ -0,0 +1,82 @@
+package com.haulmont.testtask;
+
+import com.haulmont.testtask.backend.*;
+import com.haulmont.testtask.view.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class ViewConfig {
+    @Value("${client.list.default.page.size}")
+    private int DEFAULT_PAGE_SIZE;
+    @Value("${client.list.default.sort.column}")
+    private String DEFAULT_SORT_COLUMN;
+
+    @Bean
+    @Scope("prototype")
+    ClientGridLayout clientGridLayout(ClientProvider clientProvider, CreditOfferGridLayout creditOfferGridLayout) {
+        return new ClientGridLayout(clientProvider, creditOfferGridLayout, DEFAULT_PAGE_SIZE, DEFAULT_SORT_COLUMN);
+    }
+
+    @Bean
+    @Scope("prototype")
+    CreditOfferGridLayout creditOfferGridLayout(CreditOfferProviderByClient creditOfferProviderByClient) {
+        return new CreditOfferGridLayout(creditOfferProviderByClient);
+    }
+
+    @Bean
+    @Scope("prototype")
+    PaymentGridLayout paymentGridLayout() {
+        return new PaymentGridLayout();
+    }
+
+    @Bean
+    @Scope("prototype")
+    CreateClientForm createClientForm(ClientFieldsValidator clientFieldsValidator, ClientSaver clientSaver) {
+        return new CreateClientForm(clientFieldsValidator, clientSaver);
+    }
+
+    @Bean
+    @Scope("prototype")
+    CreateCreditForm createCreditForm(BankProvider bankProvider, CreditSaver creditSaver, Validator validator) {
+        return new CreateCreditForm(bankProvider, creditSaver, validator);
+    }
+
+    @Bean
+    @Scope("prototype")
+    CreateCreditOfferForm createCreditOfferForm(CreditOfferCreator creditOfferCreator, CreditProvider creditProvider,
+                                                PaymentCalculator paymentCalculator, PaymentGridLayout paymentGridLayout,
+                                                Validator validator) {
+        return new CreateCreditOfferForm(creditOfferCreator, creditProvider, paymentCalculator, paymentGridLayout, validator);
+    }
+
+    @Bean
+    @Scope("prototype")
+    CreditGridLayout creditGridLayout(CreditProvider creditProvider) {
+        return new CreditGridLayout(creditProvider);
+    }
+
+    @Bean
+    @Scope("prototype")
+    CreateBankForm createBankForm(BankSaver bankSaver) {
+        return new CreateBankForm(bankSaver);
+    }
+
+    @Bean
+    @Scope("prototype")
+    BankGridLayout bankGridLayout(BankProvider bankProvider,
+                                  BankClientsGridLayout bankClientsGridLayout) {
+        return new BankGridLayout(bankProvider, bankClientsGridLayout);
+    }
+
+    @Bean
+    @Scope("prototype")
+    BankClientsGridLayout bankClientsGridLayout() {
+        return new BankClientsGridLayout();
+    }
+
+
+
+}
