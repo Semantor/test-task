d095271d1dc9f3ac57aa0d2d646c2790ebef6b39
fred <frederico666@yandex.ru>
2022-01-08 10:54:13 +0400

    update client specs to use in client search

--- src/main/java/com/haulmont/testtask/model/repositories/ClientSpecifications.java
+++ src/main/java/com/haulmont/testtask/model/repositories/ClientSpecifications.java
@@ -6,6 +6,20 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class ClientSpecifications {
 
+    public Specification<Client> searchClientByAllField(String keyword){
+        return getByFirstNameLike(keyword)
+                .or(getByLastNameLike(keyword))
+                .or(getByPatronymicLike(keyword))
+                .or(getByPhoneLike(keyword))
+                .or(getByEmailLike(keyword))
+                .or(getByPassportLike(keyword));
+
+    }
+
+    public Specification<Client> searchNonRemovedClientByAllField(String keyword){
+        return searchClientByAllField(keyword).and(getNonRemoved());
+    }
+
     public Specification<Client> getByPhoneExactMatch(String phone) {
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Client_.phoneNumber), phone);
     }
@@ -18,4 +32,31 @@ public class ClientSpecifications {
         return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Client_.passport), passport);
     }
 
+    public Specification<Client> getByFirstNameLike(String firstName) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(Client_.firstName), "%"+firstName+"%");
+    }
+
+    public Specification<Client> getByLastNameLike(String lastName) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(Client_.lastName), "%"+lastName+"%");
+    }
+
+    public Specification<Client> getByPatronymicLike(String patronymic) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(Client_.patronymic), "%"+patronymic+"%");
+    }
+
+    public Specification<Client> getByPhoneLike(String phone) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(Client_.phoneNumber), "%"+phone+"%");
+    }
+
+    public Specification<Client> getByEmailLike(String email) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(Client_.email), "%"+email+"%");
+    }
+
+    public Specification<Client> getByPassportLike(String passport) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(Client_.passport), "%"+passport+"%");
+    }
+
+    public Specification<Client> getNonRemoved() {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(Client_.isRemoved), false);
+    }
 }
