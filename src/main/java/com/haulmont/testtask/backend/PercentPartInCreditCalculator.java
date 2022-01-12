69b6d10f4c12aed5f51e7bcdc5f58a6c2051b44b
fred <frederico666@yandex.ru>
2022-01-13 00:55:47 +0400

    Delete PercentPartInCreditCalculator.java interface and rework tests

--- src/main/java/com/haulmont/testtask/backend/PercentPartInCreditCalculator.java
+++ src/main/java/com/haulmont/testtask/backend/PercentPartInCreditCalculator.java
@@ -1,9 +1,18 @@
 package com.haulmont.testtask.backend;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 
-public interface PercentPartInCreditCalculator {
+import static com.haulmont.testtask.Setting.*;
+
+@RequiredArgsConstructor
+@Component
+public class PercentPartInCreditCalculator {
+    private final PartOfYearCalculator partOfYearCalculator;
     /**
      * return percent for period between two payments depend on balance on start period date
      *
@@ -15,5 +24,18 @@ public interface PercentPartInCreditCalculator {
      *
      * @throws IllegalArgumentException on wrong incoming date, fe startDate > endDate, negative balance, negative rate.
      */
-    BigDecimal percentPart(LocalDate startPeriodDate, LocalDate endPeriodDate, BigDecimal rate, BigDecimal remain);
+    public BigDecimal percentPart(LocalDate startPeriodDate, LocalDate endPeriodDate, BigDecimal rate, BigDecimal remain) {
+        if (!startPeriodDate.isBefore(endPeriodDate))
+            throw new IllegalArgumentException(START_DATE_MUST_BE_EARLY_THAN_END_DATE);
+        if (rate.compareTo(BigDecimal.ZERO) < 0)
+            throw new IllegalArgumentException(CREDIT_RATE_MUST_BE_MORE_OR_EQUAL_ZERO);
+        if (remain.compareTo(BigDecimal.ZERO) < 0)
+            throw new IllegalArgumentException(CREDIT_REMAIN_BALANCE_MUST_BE_MORE_OR_EQUAL_ZERO);
+
+        double baseOfCoefficient = Double.parseDouble(rate.toString()) / PERCENT_TO_FRACTION_RATE + 1;
+        double exponentOfCoefficient = partOfYearCalculator.whatPartOfYearIsPeriod(startPeriodDate, endPeriodDate);
+
+        double percentOfRemaining = Math.pow(baseOfCoefficient, exponentOfCoefficient) - 1.;
+        return BigDecimal.valueOf(percentOfRemaining).multiply(remain).setScale(MONEY_SCALE, RoundingMode.HALF_UP);
+    }
 }
--- src/test/java/com/haulmont/testtask/backend/impls/PercentPartInCreditCalculatorImplTest.java
+++ src/test/java/com/haulmont/testtask/backend/impls/PercentPartInCreditCalculatorImplTest.java
@@ -3,144 +3,132 @@ package com.haulmont.testtask.backend.impls;
 import com.haulmont.testtask.Config;
 import com.haulmont.testtask.TestConfigForNonJpa;
 import com.haulmont.testtask.backend.PartOfYearCalculator;
+import com.haulmont.testtask.backend.PercentPartInCreditCalculator;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 @SpringBootTest(classes = {Config.class, TestConfigForNonJpa.class})
 class PercentPartInCreditCalculatorImplTest {
     @MockBean
-    PartOfYearCalculator partOfYearCalculator;
-    @Autowired
-    private PercentPartInCreditCalculatorImpl percentPart;
+    private PartOfYearCalculator partOfYearCalculator;
 
-    @Test
-    void percentPartFirst() {
-        LocalDate startDate = LocalDate.of(2010, 4, 1);
-        LocalDate endDate = LocalDate.of(2010, 5, 1);
-        Mockito.when(partOfYearCalculator.whatPartOfYearIsPeriod(startDate, endDate))
-                .thenReturn(30. / 365);
-        BigDecimal actualPercentPart = percentPart.percentPart(
-                startDate, endDate,
-                BigDecimal.TEN,
-                BigDecimal.valueOf(99545.88));
-        BigDecimal expected = BigDecimal.valueOf(782.88);
+    private PercentPartInCreditCalculator percentPart;
+
+    @BeforeEach
+    void init() {
+        percentPart = new PercentPartInCreditCalculator(partOfYearCalculator);
+    }
+
+
+    public static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.arguments(
+                        LocalDate.of(2010, 4, 1),
+                        LocalDate.of(2010, 5, 1),
+                        30. / 365,
+                        BigDecimal.TEN,
+                        BigDecimal.valueOf(99545.88),
+                        BigDecimal.valueOf(782.88)
+                ),
+                Arguments.arguments(
+                        LocalDate.of(2029, 10, 1),
+                        LocalDate.of(2029, 11, 1),
+                        31. / 365,
+                        BigDecimal.TEN,
+                        BigDecimal.valueOf(2643.54),
+                        BigDecimal.valueOf(21.49)
+                ),
+                Arguments.arguments(
+                        LocalDate.of(2029, 12, 1),
+                        LocalDate.of(2030, 1, 1),
+                        31. / 365,
+                        BigDecimal.TEN,
+                        BigDecimal.valueOf(805.34),
+                        BigDecimal.valueOf(6.55)
+                ),
+                Arguments.arguments(
+                        LocalDate.of(2029, 12, 1),
+                        LocalDate.of(2030, 1, 1),
+                        31. / 365,
+                        BigDecimal.TEN,
+                        BigDecimal.ZERO,
+                        BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP)
+                ),
+                Arguments.arguments(
+                        LocalDate.of(2029, 12, 1),
+                        LocalDate.of(2030, 1, 1),
+                        31. / 365,
+                        BigDecimal.ZERO,
+                        BigDecimal.valueOf(805.34),
+                        BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP)
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    void percentPart(LocalDate start, LocalDate end, double partOfYear, BigDecimal percent, BigDecimal remain, BigDecimal expected) {
+        Mockito.when(partOfYearCalculator.whatPartOfYearIsPeriod(start, end))
+                .thenReturn(partOfYear);
+        BigDecimal actualPercentPart = percentPart.percentPart(start, end, percent, remain);
         Assertions.assertEquals(expected, actualPercentPart);
     }
 
-    @Test
-    void percentPartSecond() {
-        LocalDate startDate = LocalDate.of(2029, 10, 1);
-        LocalDate endDate = LocalDate.of(2029, 11, 1);
-        Mockito.when(partOfYearCalculator.whatPartOfYearIsPeriod(startDate, endDate))
-                .thenReturn(31. / 365);
-        BigDecimal actualPercentPart = percentPart.percentPart(
-                startDate, endDate,
-                BigDecimal.TEN,
-                BigDecimal.valueOf(2643.54));
-        BigDecimal expected = BigDecimal.valueOf(21.49);
-        Assertions.assertEquals(expected, actualPercentPart);
+    public static Stream<Arguments> dataForExceptionProvider() {
+        return Stream.of(
+                Arguments.arguments(
+                        LocalDate.of(2030, 1, 1),
+                        LocalDate.of(2029, 12, 1),
+                        31. / 365,
+                        BigDecimal.TEN,
+                        BigDecimal.valueOf(805.34)
+                ),
+                Arguments.arguments(
+                        LocalDate.of(2029, 12, 1),
+                        LocalDate.of(2029, 12, 1),
+                        31. / 365,
+                        BigDecimal.TEN,
+                        BigDecimal.valueOf(805.34)
+                ),
+                Arguments.arguments(
+                        LocalDate.of(2029, 12, 1),
+                        LocalDate.of(2030, 1, 1),
+                        31. / 365,
+                        BigDecimal.TEN,
+                        BigDecimal.valueOf(-805.34)
+                ),
+                Arguments.arguments(
+                        LocalDate.of(2029, 12, 1),
+                        LocalDate.of(2030, 1, 1),
+                        31. / 365,
+                        BigDecimal.TEN.negate(),
+                        BigDecimal.valueOf(805.34)
+                )
+
+        );
     }
 
-    @Test
-    void percentPartThird() {
-        LocalDate startDate = LocalDate.of(2029, 12, 1);
-        LocalDate endDate = LocalDate.of(2030, 1, 1);
-        Mockito.when(partOfYearCalculator.whatPartOfYearIsPeriod(startDate, endDate))
-                .thenReturn(31. / 365);
-        BigDecimal actualPercentPart = percentPart.percentPart(
-                startDate, endDate,
-                BigDecimal.TEN,
-                BigDecimal.valueOf(805.34));
-        BigDecimal expected = BigDecimal.valueOf(6.55);
-        Assertions.assertEquals(expected, actualPercentPart);
-    }
-
-    @Test
-    void percentPartWithZeroAmount() {
-        LocalDate startDate = LocalDate.of(2029, 12, 1);
-        LocalDate endDate = LocalDate.of(2030, 1, 1);
-        Mockito.when(partOfYearCalculator.whatPartOfYearIsPeriod(startDate, endDate))
-                .thenReturn(31. / 365);
-        BigDecimal actualPercentPart = percentPart.percentPart(
-                startDate, endDate,
-                BigDecimal.TEN,
-                BigDecimal.ZERO);
-        BigDecimal expected = BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP);
-        Assertions.assertEquals(expected, actualPercentPart);
-    }
-
-    @Test
-    void percentPartWithZeroRate() {
-        LocalDate startDate = LocalDate.of(2029, 12, 1);
-        LocalDate endDate = LocalDate.of(2030, 1, 1);
-        Mockito.when(partOfYearCalculator.whatPartOfYearIsPeriod(startDate, endDate))
-                .thenReturn(31. / 365);
-        BigDecimal actualPercentPart = percentPart.percentPart(
-                startDate, endDate,
-                BigDecimal.ZERO,
-                BigDecimal.valueOf(805.34));
-        BigDecimal expected = BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP);
-        Assertions.assertEquals(expected, actualPercentPart);
-    }
-
-    @Test
-    void percentPartThrowsIllegalArgExWhenEndDateBeforeStartDate() {
-        LocalDate endDate = LocalDate.of(2029, 12, 1);
-        LocalDate startDate = LocalDate.of(2030, 1, 1);
-        Mockito.when(partOfYearCalculator.whatPartOfYearIsPeriod(startDate, endDate))
-                .thenReturn(31. / 365);
-        BigDecimal remain = BigDecimal.valueOf(805.34);
+    @ParameterizedTest
+    @MethodSource("dataForExceptionProvider")
+    void percentPartThrowsIllegalArg(LocalDate start, LocalDate end, double partOfYear, BigDecimal percent, BigDecimal remain) {
+        Mockito.when(partOfYearCalculator.whatPartOfYearIsPeriod(start, end))
+                .thenReturn(partOfYear);
         Assertions.assertThrows(IllegalArgumentException.class, () -> percentPart.percentPart(
-                startDate, endDate,
-                BigDecimal.TEN,
+                start, end,
+                percent,
                 remain));
     }
 
-    @Test
-    void percentPartThrowsIllegalArgExWhenStartDateAndEndDateIsSameDate() {
-        LocalDate startDate = LocalDate.of(2029, 12, 1);
-        LocalDate endDate = LocalDate.of(2029, 12, 1);
-        Mockito.when(partOfYearCalculator.whatPartOfYearIsPeriod(startDate, endDate))
-                .thenReturn(31. / 365);
-        BigDecimal remain = BigDecimal.valueOf(805.34);
-        Assertions.assertThrows(IllegalArgumentException.class, () -> percentPart.percentPart(
-                startDate, endDate,
-                BigDecimal.TEN,
-                remain));
-    }
-
-    @Test
-    void percentPartThrowsIllegalArgExWithNegativeBalance() {
-        LocalDate startDate = LocalDate.of(2029, 12, 1);
-        LocalDate endDate = LocalDate.of(2030, 1, 1);
-        Mockito.when(partOfYearCalculator.whatPartOfYearIsPeriod(startDate, endDate))
-                .thenReturn(31. / 365);
-        BigDecimal remain = BigDecimal.valueOf(-805.34);
-        Assertions.assertThrows(IllegalArgumentException.class, () -> percentPart.percentPart(
-                startDate, endDate,
-                BigDecimal.TEN,
-                remain));
-    }
-
-    @Test
-    void percentPartThrowsIllegalArgExWithNegativeCreditRate() {
-        LocalDate startDate = LocalDate.of(2029, 12, 1);
-        LocalDate endDate = LocalDate.of(2030, 1, 1);
-        Mockito.when(partOfYearCalculator.whatPartOfYearIsPeriod(startDate, endDate))
-                .thenReturn(31. / 365);
-        BigDecimal remain = BigDecimal.valueOf(805.34);
-        BigDecimal creditRate = BigDecimal.TEN.negate();
-        Assertions.assertThrows(IllegalArgumentException.class, () -> percentPart.percentPart(
-                startDate, endDate,
-                creditRate,
-                remain));
-    }
 }
