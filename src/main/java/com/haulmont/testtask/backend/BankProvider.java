f44155c05b909d61d109bf16847c31b74baf3b15
fred <frederico666@yandex.ru>
2022-01-12 18:09:51 +0400

    Delete BankProvider.java interface

--- src/main/java/com/haulmont/testtask/backend/BankProvider.java
+++ src/main/java/com/haulmont/testtask/backend/BankProvider.java
@@ -1,26 +1,47 @@
 package com.haulmont.testtask.backend;
 
 import com.haulmont.testtask.model.entity.Bank;
-import org.jetbrains.annotations.Nullable;
+import com.haulmont.testtask.model.repositories.BankRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
-public interface BankProvider {
+
+@AllArgsConstructor
+@Slf4j
+@Component
+public class BankProvider {
+    private final BankRepository bankRepository;
+    private final StringUUIDHandler uuidHandler;
+
     /**
      * Provide bank buy its id from database.
      * validating incoming param
      */
-    Optional<Bank> getBank(@Nullable String uuidString);
+    public Optional<Bank> getBank(String uuidString) {
+        UUID bankUUID = uuidHandler.validateStringUUIDAndReturnNullOrUUID(uuidString);
+        if (bankUUID == null) return Optional.empty();
+        return bankRepository.findById(bankUUID);
+    }
 
     /**
      * @return all banks list or {@link Collections#emptyList()} due to some problem
      */
-    List<Bank> getAllBanks();
+    public List<Bank> getAllBanks() {
+        return bankRepository.findAll();
+    }
+
 
     /**
      * add or update to target bank all its clients in {@link Bank#getClients()} field
      */
-    void updateClients(Bank bank);
+    public void updateClients(Bank bank) {
+        if (bank == null || bank.getBankId() == null || bankRepository.findById(bank.getBankId()).isEmpty()) return;
+        bank.setClients(bankRepository.getBankClients(bank));
+    }
 }
