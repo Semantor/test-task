1975f3394539ec754872f45d254baeaf97127836
fred <frederico666@yandex.ru>
2022-01-12 22:56:23 +0400

    Delete CreditOfferProviderByClient interface and redundant implementation

--- src/main/java/com/haulmont/testtask/backend/CreditOfferProviderByClient.java
+++ src/main/java/com/haulmont/testtask/backend/CreditOfferProviderByClient.java
@@ -1,19 +1,32 @@
 package com.haulmont.testtask.backend;
 
 import com.haulmont.testtask.model.entity.Client;
-import com.haulmont.testtask.model.entity.Credit;
 import com.haulmont.testtask.model.entity.CreditOffer;
+import com.haulmont.testtask.model.repositories.ClientRepository;
+import com.haulmont.testtask.model.repositories.CreditOfferRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.Nullable;
+import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 
-public interface CreditOfferProviderByClient {
+@AllArgsConstructor
+@Slf4j
+@Component
+public class CreditOfferProviderByClient {
+    private final CreditOfferRepository creditOfferRepository;
+    private final ClientRepository clientRepository;
 
     /**
      * validate client
      * return {@link Collections#emptyList()} on fail
      */
-    List<CreditOffer> getByClient(@Nullable Client client);
+    public List<CreditOffer> getByClient(@Nullable Client client) {
+        if (client == null || client.getClientId() == null || clientRepository.findById(client.getClientId()).isEmpty())
+            return Collections.emptyList();
+        return creditOfferRepository.findByIsCanceledAndClient(false, client);
+    }
 
 }
