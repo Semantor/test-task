5ddba712241864202238336e8ee06245e7063c21
fred <frederico666@yandex.ru>
2022-01-12 20:13:53 +0400

    Delete ClientFieldAvailabilityChecker interface

--- src/main/java/com/haulmont/testtask/backend/ClientFieldAvailabilityChecker.java
+++ src/main/java/com/haulmont/testtask/backend/ClientFieldAvailabilityChecker.java
@@ -1,9 +1,25 @@
 package com.haulmont.testtask.backend;
 
-public interface ClientFieldAvailabilityChecker {
-    boolean isAvailablePhone(String phone);
+import com.haulmont.testtask.model.repositories.ClientRepository;
+import com.haulmont.testtask.model.repositories.ClientSpecifications;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
-    boolean isAvailableEmail(String email);
+@RequiredArgsConstructor
+@Component
+public class ClientFieldAvailabilityChecker {
+    private final ClientRepository clientRepository;
+    private final ClientSpecifications clientSpecifications;
 
-    boolean isAvailablePassport(String passport);
+    public boolean isAvailablePhone(String phone) {
+        return clientRepository.findAll(clientSpecifications.getByPhoneExactMatch(phone)).isEmpty();
+    }
+
+    public boolean isAvailableEmail(String email) {
+        return clientRepository.findAll(clientSpecifications.getByEmailExactMatch(email)).isEmpty();
+    }
+
+    public boolean isAvailablePassport(String passport) {
+        return clientRepository.findAll(clientSpecifications.getByPassportExactMatch(passport)).isEmpty();
+    }
 }
