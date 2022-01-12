5996edf82ad568edee637f000683ff044af0a232
fred <frederico666@yandex.ru>
2022-01-13 00:20:01 +0400

    Delete PartOfYearCalculator interface and update test

--- src/main/java/com/haulmont/testtask/backend/PartOfYearCalculator.java
+++ src/main/java/com/haulmont/testtask/backend/PartOfYearCalculator.java
@@ -1,18 +1,36 @@
 package com.haulmont.testtask.backend;
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDate;
 
-public interface PartOfYearCalculator {
+@Component
+public class PartOfYearCalculator {
     /**
      * return what part of year is period between two incoming dates;
      * difference between these dates must be less than year;
      * must be work if dates reside in different years(December - January).
      * must consider if year leap or not.
      *
-     * @param passedDate start period date
-     * @param nextDate   end period date
+     * @param startPeriodDate start period date
+     * @param endPeriodDate   end period date
      * @return double value of fraction
      */
-    double whatPartOfYearIsPeriod(LocalDate passedDate, LocalDate nextDate);
+    public double whatPartOfYearIsPeriod(LocalDate startPeriodDate, LocalDate endPeriodDate) {
+        int dayOfYearOnPaymentDate = endPeriodDate.getDayOfYear();
+        int dayOfYearOnPassedPaymentDate = startPeriodDate.getDayOfYear();
+
+        if (dayOfYearOnPaymentDate > dayOfYearOnPassedPaymentDate) {
+            int periodLength = dayOfYearOnPaymentDate - dayOfYearOnPassedPaymentDate;
+            int yearLength = startPeriodDate.lengthOfYear();
+            return periodLength / (double) yearLength;
+        }
+        int periodLengthInPassedYear = startPeriodDate.lengthOfYear() - startPeriodDate.getDayOfYear();
+
+        double partOfThePassedYear = periodLengthInPassedYear / (double) startPeriodDate.lengthOfYear();
+        double partOfNewYear = endPeriodDate.getDayOfYear() / (double) endPeriodDate.lengthOfYear();
+
+        return partOfThePassedYear + partOfNewYear;
+    }
 
 }
--- src/test/java/com/haulmont/testtask/backend/impls/PartOfYearCalculatorImplTest.java
+++ src/test/java/com/haulmont/testtask/backend/impls/PartOfYearCalculatorImplTest.java
@@ -2,55 +2,44 @@ package com.haulmont.testtask.backend.impls;
 
 import com.haulmont.testtask.Config;
 import com.haulmont.testtask.TestConfigForNonJpa;
+import com.haulmont.testtask.backend.PartOfYearCalculator;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 @SpringBootTest(classes = {Config.class, TestConfigForNonJpa.class})
 class PartOfYearCalculatorImplTest {
-    @Autowired
-    private PartOfYearCalculatorImpl partOfYearCalculator;
 
-    @Test
-    void whatPartOfYearIsPeriodBetweenTwoMonthInCommonYear() {
-        double actualPart = partOfYearCalculator.whatPartOfYearIsPeriod(
-                LocalDate.of(2010, 1, 1),
-                LocalDate.of(2010, 2, 1)
+    private final PartOfYearCalculator partOfYearCalculator
+            = new PartOfYearCalculator();
+    private final double scale = 0.00000001;
+
+    public static Stream<Arguments> periodProvider() {
+        return Stream.of(
+                Arguments.arguments(LocalDate.of(2010, 1, 1),
+                        LocalDate.of(2010, 2, 1),
+                        31. / 365),
+                Arguments.arguments(LocalDate.of(2000, 1, 1),
+                        LocalDate.of(2000, 2, 1),
+                        31. / 366),
+                Arguments.arguments(LocalDate.of(2021, 12, 11),
+                        LocalDate.of(2022, 1, 11),
+                        31. / 365),
+                Arguments.arguments(LocalDate.of(1999, 12, 11),
+                        LocalDate.of(2000, 1, 11),
+                        20. / 365 + 11. / 366)
         );
-        double expectedPart = 31./365;
-        Assertions.assertEquals(expectedPart,actualPart);
     }
 
-    @Test
-    void whatPartOfYearIsPeriodBetweenTwoMonthInLearYear() {
-        double actualPart = partOfYearCalculator.whatPartOfYearIsPeriod(
-                LocalDate.of(2000, 1, 1),
-                LocalDate.of(2000, 2, 1)
-        );
-        double expectedPart = 31./366;
-        Assertions.assertEquals(expectedPart,actualPart);
-    }
-
-    @Test
-    void whatPartOfYearIsPeriodBetweenDecemberAndJanuary() {
-        double actualPart = partOfYearCalculator.whatPartOfYearIsPeriod(
-                LocalDate.of(2021, 12, 11),
-                LocalDate.of(2022, 1, 11)
-        );
-        double expectedPart = 31./365;
-        Assertions.assertEquals(expectedPart,actualPart);
-    }
-
-    @Test
-    void whatPartOfYearIsPeriodBetweenDecemberAndJanuaryWhenOneYearIsLeap() {
-        double actualPart = partOfYearCalculator.whatPartOfYearIsPeriod(
-                LocalDate.of(1999, 12, 11),
-                LocalDate.of(2000, 1, 11)
-        );
-        double expectedPart = 20./365 + 11./366;
-        Assertions.assertEquals(expectedPart,actualPart);
+    @ParameterizedTest
+    @MethodSource("periodProvider")
+    void whatPartOfYearIsPeriod(LocalDate start, LocalDate end, double expected) {
+        double actualPart = partOfYearCalculator.whatPartOfYearIsPeriod(start, end);
+        Assertions.assertTrue(Math.abs(expected - actualPart) < scale);
     }
 }
