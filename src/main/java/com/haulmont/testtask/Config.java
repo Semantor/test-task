505987f6e5e07bdf5cdc2bf3ad2e404f6ea9b8da
fred <frederico666@yandex.ru>
2022-01-12 17:37:27 +0400

    create creditConstraintProvider from properties in Config

--- src/main/java/com/haulmont/testtask/Config.java
+++ src/main/java/com/haulmont/testtask/Config.java
@@ -1,35 +1,29 @@
 package com.haulmont.testtask;
 
+import com.haulmont.testtask.backend.CreditConstraintProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
-import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 
 @SpringBootConfiguration
 @PropertySource("classpath:config.properties")
 public class Config {
 
-    public static BigDecimal CREDIT_LIMIT_MAX_VALUE;
-    public static BigDecimal CREDIT_LIMIT_MIN_VALUE;
-    public static BigDecimal CREDIT_RATE_MAX_VALUE;
-    public static BigDecimal CREDIT_RATE_MIN_VALUE;
 
     @Value("${max.credit.limit}")
-    private String maxCreditLimit;
+    private BigDecimal maxCreditLimit;
     @Value("${min.credit.limit}")
-    private String minCreditLimit;
+    private BigDecimal minCreditLimit;
     @Value("${max.credit.rate}")
-    private String maxCreditRate;
+    private BigDecimal maxCreditRate;
     @Value("${min.credit.rate}")
-    private String minCreditRate;
+    private BigDecimal minCreditRate;
 
-    @PostConstruct
-    private void init() {
-        CREDIT_LIMIT_MAX_VALUE = new BigDecimal(maxCreditLimit);
-        CREDIT_LIMIT_MIN_VALUE = new BigDecimal(minCreditLimit);
-        CREDIT_RATE_MAX_VALUE = new BigDecimal(maxCreditRate);
-        CREDIT_RATE_MIN_VALUE = new BigDecimal(minCreditRate);
+    @Bean
+    CreditConstraintProvider creditConstraintProvider() {
+        return new CreditConstraintProvider(maxCreditLimit, minCreditLimit, maxCreditRate, minCreditRate);
     }
 }
