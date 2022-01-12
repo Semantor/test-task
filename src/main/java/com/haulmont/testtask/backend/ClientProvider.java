1edd1551f9771b166ecdb2c19e5bd28b61553bce
fred <frederico666@yandex.ru>
2022-01-12 20:20:38 +0400

    Delete ClientProvider interface and redundant implementation

--- src/main/java/com/haulmont/testtask/backend/ClientProvider.java
+++ src/main/java/com/haulmont/testtask/backend/ClientProvider.java
@@ -1,17 +1,32 @@
 package com.haulmont.testtask.backend;
 
 import com.haulmont.testtask.model.entity.Client;
+import com.haulmont.testtask.model.repositories.ClientRepository;
+import lombok.AllArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
-public interface ClientProvider {
+@AllArgsConstructor
+@Component
+public class ClientProvider {
+    private final ClientRepository clientRepository;
+    private final StringUUIDHandler stringUUIDHandler;
+
     /**
      * return all available clients (non-removed)
      */
-    List<Client> getClients();
+    public List<Client> getClients() {
+        return clientRepository.findByIsRemoved(false);
+    }
+
 
     /**
      * trying to return list of client using pagination
@@ -29,11 +44,20 @@ public interface ClientProvider {
      * @param pageNumber number of page
      * @param sort       sorting field
      */
-    List<Client> getClients(int pageSize, int pageNumber, @NotNull String sort);
+    public List<Client> getClients(int pageSize, int pageNumber, @NotNull String sort) {
+        if (pageSize < 1 || pageNumber < 0) return Collections.emptyList();
+        return clientRepository.findByIsRemoved(false, PageRequest.of(pageNumber, pageSize, Sort.by(sort))).toList();
+    }
 
     /**
-     * trying to return client by client id
+     * trying to return non-removed client by client id
      * validate uuid
      */
-    Optional<Client> getClient(@Nullable String uuidString);
+    public Optional<Client> getClient(@Nullable String uuidString) {
+        UUID uuid = stringUUIDHandler.validateStringUUIDAndReturnNullOrUUID(uuidString);
+        if (uuid == null) return Optional.empty();
+        Optional<Client> byId = clientRepository.findById(uuid);
+        if (byId.isEmpty()) return Optional.empty();
+        return byId.get().isRemoved() ? Optional.empty() : byId;
+    }
 }
