7776c0852ac06cf3c57c8dfc453953d67574d945
fred <frederico666@yandex.ru>
2022-01-12 20:12:03 +0400

    Delete BankSaver interface

--- src/main/java/com/haulmont/testtask/backend/BankSaver.java
+++ src/main/java/com/haulmont/testtask/backend/BankSaver.java
@@ -1,9 +1,15 @@
 package com.haulmont.testtask.backend;
 
 import com.haulmont.testtask.model.entity.Bank;
-import org.jetbrains.annotations.Nullable;
+import com.haulmont.testtask.model.repositories.BankRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
 
-public interface BankSaver {
+@AllArgsConstructor
+@Component
+public class BankSaver {
+    private final BankRepository bankRepository;
+
     /**
      * trying to save bank in db
      * validate it
@@ -11,5 +17,8 @@ public interface BankSaver {
      *
      * @throws com.haulmont.testtask.backend.excs.CreateBankException due to fail
      */
-    void save(@Nullable Bank bank);
+    public void save(Bank bank) {
+        if (bank == null) return;
+        bankRepository.save(bank);
+    }
 }
