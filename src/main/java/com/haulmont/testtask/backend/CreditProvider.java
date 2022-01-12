62603b4396bef33f2413e3504ea689b440526133
fred <frederico666@yandex.ru>
2022-01-12 23:07:36 +0400

    Delete CreditProvider interface and redundant implementation

--- src/main/java/com/haulmont/testtask/backend/CreditProvider.java
+++ src/main/java/com/haulmont/testtask/backend/CreditProvider.java
@@ -1,17 +1,33 @@
 package com.haulmont.testtask.backend;
 
 import com.haulmont.testtask.model.entity.Credit;
+import com.haulmont.testtask.model.repositories.CreditRepository;
+import lombok.AllArgsConstructor;
 import org.jetbrains.annotations.Nullable;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
-public interface CreditProvider {
+@AllArgsConstructor
+@Component
+public class CreditProvider {
+    private final CreditRepository creditRepository;
+    private final StringUUIDHandler stringUUIDHandler;
 
-    List<Credit> getAllCredit();
+    public List<Credit> getAllCredit() {
+        return creditRepository.findByIsUnused(false);
+    }
 
     /**
      * validate UUID
      */
-    Optional<Credit> getCredit(@Nullable String uuidString);
+    public Optional<Credit> getCredit(@Nullable String uuidString) {
+        UUID uuid = stringUUIDHandler.validateStringUUIDAndReturnNullOrUUID(uuidString);
+        if (uuid == null) return Optional.empty();
+        Optional<Credit> byId = creditRepository.findById(uuid);
+        if (byId.isEmpty() || byId.get().isUnused()) return Optional.empty();
+        return byId;
+    }
 }
