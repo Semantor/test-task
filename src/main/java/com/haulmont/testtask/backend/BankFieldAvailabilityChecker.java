d1e7b11e90f0c3b776261db8808bd4a0305edbbc
fred <frederico666@yandex.ru>
2022-01-12 17:55:08 +0400

    Delete BankFieldAvailabilityChecker interface

--- src/main/java/com/haulmont/testtask/backend/BankFieldAvailabilityChecker.java
+++ src/main/java/com/haulmont/testtask/backend/BankFieldAvailabilityChecker.java
@@ -1,13 +1,21 @@
 package com.haulmont.testtask.backend;
 
+import com.haulmont.testtask.model.repositories.BankRepository;
+import lombok.AllArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
 
 /**
  * check for availability field value
  * return false if this value already in used or incorrect
  * using for real time computing in client form validation
  */
-public interface BankFieldAvailabilityChecker {
-    boolean isAvailableName(@NotNull String bankName);
+@Component
+@AllArgsConstructor
+public class BankFieldAvailabilityChecker {
+    private final BankRepository bankRepository;
 
+    public boolean isAvailableName(@NotNull String bankName) {
+        return bankRepository.findByName(bankName).isEmpty();
+    }
 }
