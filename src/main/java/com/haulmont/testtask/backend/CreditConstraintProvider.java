bf4028510b0d2cf7e874450de7844749458cfdb4
fred <frederico666@yandex.ru>
2022-01-12 17:18:23 +0400

    create class to provide Credit Constraints

---
+++ src/main/java/com/haulmont/testtask/backend/CreditConstraintProvider.java
@@ -0,0 +1,13 @@
+package com.haulmont.testtask.backend;
+
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+public class CreditConstraintProvider {
+    public final BigDecimal CREDIT_LIMIT_MAX_VALUE;
+    public final BigDecimal CREDIT_LIMIT_MIN_VALUE;
+    public final BigDecimal CREDIT_RATE_MAX_VALUE;
+    public final BigDecimal CREDIT_RATE_MIN_VALUE;
+}
