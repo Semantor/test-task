92abfb6d0dcb1739aea125ef2592a39eb9bbb5b9
fred <frederico666@yandex.ru>
2022-01-14 17:45:05 +0400

    update CreditLimitConstraintValidator make response dynamic

--- src/main/java/com/haulmont/testtask/backend/util/CreditLimitConstraintValidator.java
+++ src/main/java/com/haulmont/testtask/backend/util/CreditLimitConstraintValidator.java
@@ -1,7 +1,9 @@
 package com.haulmont.testtask.backend.util;
 
+import com.haulmont.testtask.Setting;
 import com.haulmont.testtask.backend.CreditConstraintProvider;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraintvalidation.HibernateConstraintValidatorContext;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,15 +13,27 @@ import java.math.BigDecimal;
 public class CreditLimitConstraintValidator implements ConstraintValidator<CreditLimitConstraint, BigDecimal> {
     private final CreditConstraintProvider creditConstraintProvider;
 
+
     @Override
     public void initialize(CreditLimitConstraint constraintAnnotation) {
-
+        ConstraintValidator.super.initialize(constraintAnnotation);
     }
 
     @Override
     public boolean isValid(BigDecimal value, ConstraintValidatorContext context) {
-        if (value == null) return false;
-        return value.compareTo(creditConstraintProvider.CREDIT_LIMIT_MIN_VALUE) > -1 &&
+        HibernateConstraintValidatorContext hibernateContext = context.unwrap(HibernateConstraintValidatorContext.class);
+        boolean isValid = value != null && value.compareTo(creditConstraintProvider.CREDIT_LIMIT_MIN_VALUE) > -1 &&
                 value.compareTo(creditConstraintProvider.CREDIT_LIMIT_MAX_VALUE) < 1;
+        if (!isValid) {
+            hibernateContext.disableDefaultConstraintViolation();
+            hibernateContext
+                    .buildConstraintViolationWithTemplate(
+                            Setting.WRONG_AMOUNT_MUST_BE_MORE_OR_EQUAL +
+                                    creditConstraintProvider.CREDIT_LIMIT_MIN_VALUE +
+                                    Setting.AND_LESS_THEN +
+                                    creditConstraintProvider.CREDIT_LIMIT_MAX_VALUE)
+                    .addConstraintViolation();
+        }
+        return isValid;
     }
 }
