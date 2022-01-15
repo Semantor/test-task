dc2e75a2f59c9ab8c9b0146878074900ce6dee94
fred <frederico666@yandex.ru>
2022-01-15 18:03:18 +0400

    delete redundant exception

--- src/main/java/com/haulmont/testtask/backend/excs/CreateBankException.java
+++
@@ -1,7 +0,0 @@
-package com.haulmont.testtask.backend.excs;
-
-public class CreateBankException extends RuntimeException {
-    public CreateBankException(String message) {
-        super(message);
-    }
-}
