ec2df4f7c915c6ca1eb3bde93ed9b45ea6b73e1d
fred <frederico666@yandex.ru>
2022-01-22 23:04:54 +0400

    rewrite bean using CGlib

--- app/src/main/java/com/haulmont/testtask/Config.java
+++ app/src/main/java/com/haulmont/testtask/Config.java
@@ -1,10 +1,14 @@
 package com.haulmont.testtask;
 
+import com.haulmont.testtask.api.ClientsApiController;
+import com.haulmont.testtask.api.ClientsApiDelegate;
 import com.haulmont.testtask.backend.CreditConstraintProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 import java.math.BigDecimal;
 
@@ -29,5 +33,11 @@ public class Config {
     @Bean
     CreditConstraintProvider creditConstraintProvider() {
         return new CreditConstraintProvider(maxCreditLimit, minCreditLimit, maxCreditRate, minCreditRate,monthMaxValue,monthMinValue);
+    }
+
+    @Bean
+    @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+    ClientsApiController clientsApiController(ClientsApiDelegate clientsApiDelegate){
+        return new ClientsApiController(clientsApiDelegate);
     }
 }
