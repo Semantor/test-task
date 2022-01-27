fd984d3945ab5bc6b12758581273e447e4fe6e6e
fred <frederico666@yandex.ru>
2022-01-27 12:35:37 +0400

    create Services for new REST endpoints

--- app/src/main/java/com/haulmont/testtask/backend/RestNonRemovedClientsProvider.java
+++ app/src/main/java/com/haulmont/testtask/backend/RestNonRemovedClientsProvider.java
@@ -1,9 +1,9 @@
 package com.haulmont.testtask.backend;
 
+import com.haulmont.testtask.backend.util.ClientExchanger;
 import com.haulmont.testtask.model.dto.ClientDTO;
 import com.haulmont.testtask.model.entity.Client;
 import com.haulmont.testtask.model.repositories.ClientRepository;
-import com.haulmont.testtask.settings.ErrorMessages;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -19,21 +19,13 @@ import static com.haulmont.testtask.settings.ErrorMessages.SOME_DB_PROBLEM;
 @Slf4j
 public class RestNonRemovedClientsProvider {
     private final ClientRepository clientRepository;
+    private final ClientExchanger clientExchanger;
 
     public List<ClientDTO> getClients() {
         List<ClientDTO> resultList = new ArrayList<>();
         try {
-            for (Client client : clientRepository.findByIsRemoved(false)) {
-                resultList.add(new ClientDTO()
-                        .firstname(client.getFirstName())
-                        .lastname(client.getLastName())
-                        .patronymic(client.getPatronymic())
-                        .phone(client.getPhoneNumber())
-                        .email(client.getEmail())
-                        .passport(client.getPassport())
-                );
-            }
-
+            for (Client client : clientRepository.findByIsRemoved(false))
+                resultList.add(clientExchanger.exchangeToDTO(client));
         } catch (Exception exception) {
             log.warn(SOME_DB_PROBLEM);
             return Collections.emptyList();
